0ae52ea1127cf19a1ee633c0f23096b084be0aee
elkinmercado <elkin_mesi@hotmail.com>
2021-02-26 00:04:44 -0500

    ajustes

--- app/src/main/java/com/example/myapplicationprueba/MainActivity.java
+++ app/src/main/java/com/example/myapplicationprueba/MainActivity.java
@@ -10,7 +10,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadData();
     }
 
     private void parseData(){
@@ -33,10 +37,27 @@ public class MainActivity extends AppCompatActivity {
 
         Retrofit retrofit2 = new Retrofit.Builder()
                 .baseUrl("https://community-open-weather-map.p.rapidapi.com/")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         WeatherServices service = retrofit2.create(WeatherServices.class);
        Call<Main> data=service.CurrentWeatherData("London,uk");
+       data.enqueue(new Callback<Main>() {
+                        @Override
+                        public void onResponse(Call<Main> call, Response<Main> response) {
+                                if(response.isSuccessful()){
+                                    System.out.println(response.body().getTemp_max());
+
+                                }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Main> call, Throwable t) {
+
+                        }
+                    }
+
+       );
 
     }
 }
--- app/src/main/java/com/example/myapplicationprueba/Retrofit/WeatherServices.java
+++ app/src/main/java/com/example/myapplicationprueba/Retrofit/WeatherServices.java
@@ -5,10 +5,15 @@ import com.example.myapplicationprueba.models.Main;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 public interface WeatherServices {
-
+    @Headers({
+            "x-rapidapi-key: 9efcf42886mshfc9edc7e97eaa8dp123ddfjsn80941e4feb6c",
+            "x-rapidapi-host: community-open-weather-map.p.rapidapi.com",
+            "useQueryString:true"
+    })
     @GET("weather")
     Call<Main> CurrentWeatherData(@Query("q") String location);
 
